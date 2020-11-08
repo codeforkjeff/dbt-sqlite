022c60103de216bc14f8dfb3a4e59f4eb7c3ae31
codeforkjeff <jeff@codefork.com>
2020-11-08 00:27:28 -0800

    put hex() around md5() so column values are more readable and we avoid blob handling issues

---
+++ dbt/include/sqlite/macros/materializations/snapshot/strategies.sql
@@ -0,0 +1,6 @@
+{% macro default__snapshot_hash_arguments(args) -%}
+    hex(md5({%- for arg in args -%}
+        coalesce(cast({{ arg }} as varchar ), '')
+        {% if not loop.last %} || '|' || {% endif %}
+    {%- endfor -%}))
+{%- endmacro %}
