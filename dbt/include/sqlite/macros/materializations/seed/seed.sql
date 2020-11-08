253fbcd53afe1a56b2ba7ac6d30dd5cd64468401
codeforkjeff <jeff@codefork.com>
2020-11-07 23:08:25 -0800

    rm override made obsolete by SQLiteRelation

--- dbt/include/sqlite/macros/materializations/seed/seed.sql
+++ dbt/include/sqlite/macros/materializations/seed/seed.sql
@@ -1,27 +1,4 @@
 
-{% macro sqlite__create_csv_table(model, agate_table) %}
-  {%- set column_override = model['config'].get('column_types', {}) -%}
-  {%- set quote_seed_column = model['config'].get('quote_columns', None) -%}
-
-  {% set sql %}
-    {# leave off 'database' #}
-    create table {{ this.schema }}.{{ this.identifier }} (
-        {%- for col_name in agate_table.column_names -%}
-            {%- set inferred_type = adapter.convert_type(agate_table, loop.index0) -%}
-            {%- set type = column_override.get(col_name, inferred_type) -%}
-            {%- set column_name = (col_name | string) -%}
-            {{ adapter.quote_seed_column(column_name, quote_seed_column) }} {{ type }} {%- if not loop.last -%}, {%- endif -%}
-        {%- endfor -%}
-    )
-  {% endset %}
-
-  {% call statement('_') -%}
-    {{ sql }}
-  {%- endcall %}
-
-  {{ return(sql) }}
-{% endmacro %}
-
 {% macro sqlite__load_csv_rows(model, agate_table) %}
     {% set batch_size = 100000 %}
     {% set cols_sql = get_seed_column_quoted_csv(model, agate_table.column_names) %}
