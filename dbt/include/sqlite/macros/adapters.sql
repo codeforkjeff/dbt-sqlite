2bcfb8920e807f0e061697abd78381f57c6c323d
codeforkjeff <jeff@codefork.com>
2021-08-10 16:15:26 -0700

    move macro into separate test materialization file

--- dbt/include/sqlite/macros/adapters.sql
+++ dbt/include/sqlite/macros/adapters.sql
@@ -116,16 +116,3 @@ that here when making the relation and everything else should Just Work
 
     {% do return(tmp_relation) %}
 {% endmacro %}
-
-{% macro sqlite__get_test_sql(main_sql, fail_calc, warn_if, error_if, limit) -%}
-    select
-      {{ fail_calc }} as failures,
-      case when {{ fail_calc }} {{ warn_if }}
-        then 'true' else 'false' end as should_warn,
-      case when {{ fail_calc }} {{ error_if }}
-        then 'true' else 'false' end as should_error
-    from (
-      {{ main_sql }}
-      {{ "limit " ~ limit if limit != none }}
-    ) dbt_internal_test
-{%- endmacro %}
