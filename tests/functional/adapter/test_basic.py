73e4463c9998c140335135eeb0a770b2e3cee68e
codeforkjeff <jeff@codefork.com>
2022-07-16 10:04:44 -0700

    add two new (failing) test classes, marked as skip for now

--- tests/functional/adapter/test_basic.py
+++ tests/functional/adapter/test_basic.py
@@ -4,6 +4,7 @@ import os
 
 import pytest
 
+from dbt.tests.adapter.basic.expected_catalog import base_expected_catalog, no_stats, expected_references_catalog
 from dbt.tests.adapter.basic.test_base import BaseSimpleMaterializations
 from dbt.tests.adapter.basic.test_singular_tests import BaseSingularTests
 from dbt.tests.adapter.basic.test_singular_tests_ephemeral import BaseSingularTestsEphemeral
@@ -14,9 +15,7 @@ from dbt.tests.adapter.basic.test_generic_tests import BaseGenericTests
 from dbt.tests.adapter.basic.test_snapshot_check_cols import BaseSnapshotCheckCols
 from dbt.tests.adapter.basic.test_snapshot_timestamp import BaseSnapshotTimestamp
 from dbt.tests.adapter.basic.test_adapter_methods import BaseAdapterMethod
-from dbt.tests.adapter.basic.test_docs_generate import BaseDocsGenerate
-from dbt.tests.adapter.basic.test_docs_generate import BaseDocsGenReferences
-
+from dbt.tests.adapter.basic.test_docs_generate import BaseDocsGenerate, BaseDocsGenReferences
 
 
 class TearDownMixin():
@@ -66,9 +65,37 @@ class TestBaseAdapterMethodSqlite(BaseAdapterMethod, TearDownMixin):
     pass
 
 
+@pytest.mark.skip("TODO: views can't reference views in other schemas; make our own customized fixtures and expectations")
 class TestDocsGenerateSqlite(BaseDocsGenerate, TearDownMixin):
-    pass
+    @pytest.fixture(scope="class")
+    def expected_catalog(self, project):
+        return base_expected_catalog(
+            project,
+            role=None,
+            id_type="integer",
+            text_type="text",
+            time_type="DATETIME",
+            view_type="view",
+            table_type="table",
+            model_stats=no_stats(),
+            seed_stats=no_stats(),
+        )
 
 
-class TestDocsGenerateSqlite(BaseDocsGenReferences, TearDownMixin):
-    pass
+@pytest.mark.skip('TODO: figure out error with column indices not lining up for seed')
+class TestDocsGenReferencesSqlite(BaseDocsGenReferences, TearDownMixin):
+    @pytest.fixture(scope="class")
+    def expected_catalog(self, project):
+        return expected_references_catalog(
+            project,
+            role=None,
+            id_type="integer",
+            text_type="text",
+            time_type="DATETIME",
+            bigint_type="bigint",
+            view_type="view",
+            table_type="table",
+            model_stats=no_stats(),
+            seed_stats=no_stats(),
+            view_summary_stats=no_stats(),
+        )
