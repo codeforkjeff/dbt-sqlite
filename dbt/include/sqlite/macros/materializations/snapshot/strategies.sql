1f52e8a41a4ffd68bcb7e27f5fb17bcb403f726c
codeforkjeff <jeff@codefork.com>
2022-07-19 18:05:23 -0700

    fix typo: don't override default

--- dbt/include/sqlite/macros/materializations/snapshot/strategies.sql
+++ dbt/include/sqlite/macros/materializations/snapshot/strategies.sql
@@ -1,4 +1,4 @@
-{% macro default__snapshot_hash_arguments(args) -%}
+{% macro sqlite__snapshot_hash_arguments(args) -%}
     hex(md5({%- for arg in args -%}
         coalesce(cast({{ arg }} as varchar ), '')
         {% if not loop.last %} || '|' || {% endif %}
